9ff36a3e8fbd63fa74e51672c54965d276f91213
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2009-12-24 15:16:01 +0000

    upload the fork of z3ext

---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.2.0'
+
+setup(name='zojax.widget.tinymce',
+      version=version,
+      description="Tiny MCE HTML Editor Widget for zojax",
+      long_description=(
+          'Detailed Documentation\n' +
+          '======================\n\n' +
+          'This package contains a HTML editing widget using the TinyMCE JavaScript Library.\n' +
+          'It is intended to provide an alternative to the ExtJS Editor Widget in zojax.\n\n' +
+          'This work is based on other implementations of TinyMCE widget in Zope/Python, in\n' +
+          'particular hurry.tinymce.\n\n\n' +
+          read('CHANGES.txt')
+          ),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+      keywords='',
+      author='Kevin Gill',
+      author_email='kevin.gill@openapp.ie',
+      url='http://zojax.net/',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'':'src'},
+      namespace_packages=['zojax', 'zojax.widget'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = ['setuptools',
+                          'z3c.form',
+                          'zojax.resource',
+                          'zojax.resourcepackage',
+                          'zojax.content.attachment',
+                          'zojax.richtext',
+                          ],
+      extras_require = dict(test=['zope.app.testing',
+                                  'zope.app.zcmlfiles',
+                                  'zope.testing',
+                                  'zope.testbrowser',
+                                  'zope.securitypolicy',
+                                  ]),
+      )
---
+++ src/zojax/widget/tinymce/attachments.py
@@ -0,0 +1,69 @@
+"""Adapter to get the image list for an object - these are returned
+to the tiny widget for insertion in the document
+
+This is based on zojax.content.attachment.browser.jsapy
+"""
+
+from simplejson import JSONEncoder
+
+from zope.publisher.browser import BrowserPage
+from zope.traversing.api import getName
+from zope.component import getUtility
+from zope.proxy import removeAllProxies
+from zope.app.intid.interfaces import IIntIds
+
+from zojax.content.attachment.interfaces import IImage, IAttachmentsExtension, ILink, IFile
+
+encoder = JSONEncoder()
+
+class ImageList(BrowserPage):
+
+    def __call__(self):
+        ids = getUtility(IIntIds)
+
+        data = []
+        for name, image in IAttachmentsExtension(self.context).items():
+            if IImage.providedBy(image) and image:
+                id = ids.queryId(removeAllProxies(image))
+                title = image.title or getName(image)
+                data.append((title,  '++attachment++%s' % id))
+
+        data.sort()
+        js_encoded = encoder.encode(data)
+
+        return """tinyMCEImageList = new Array(%s);""" % js_encoded[1:-1]
+
+class LinkList(BrowserPage):
+
+    def __call__(self):
+        ids = getUtility(IIntIds)
+
+        data = []
+        for name, link in IAttachmentsExtension(self.context).items():
+            if ILink.providedBy(link) and link:
+                title = link.title or getName(link)
+                data.append((title,  link.url))
+
+        data.sort()
+        js_encoded = encoder.encode(data)
+
+        return """tinyMCELinkList = new Array(%s);""" % js_encoded[1:-1]
+
+
+class FileList(BrowserPage):
+    # I assume that attached files are media files
+
+    def __call__(self):
+        ids = getUtility(IIntIds)
+
+        data = []
+        for name, file in IAttachmentsExtension(self.context).items():
+            if IFile.providedBy(file) and file:
+                id = ids.queryId(removeAllProxies(file))
+                title = file.title or getName(file)
+                data.append((title,  '++attachment++%s' % id))
+
+        data.sort()
+        js_encoded = encoder.encode(data)
+
+        return """tinyMCEMediaList = new Array(%s);""" % js_encoded[1:-1]

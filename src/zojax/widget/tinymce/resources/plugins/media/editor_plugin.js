70bd03ceed98827990392334287c529c1a2b789e
Dmitry Suvorov <suvdim@gmail.com>
2015-12-10 09:44:33 +0100

    #822: fixed the issue related to 'indexOf' property

--- src/zojax/widget/tinymce/resources/plugins/media/editor_plugin.js
+++ src/zojax/widget/tinymce/resources/plugins/media/editor_plugin.js
@@ -1681,13 +1681,4 @@
     tinymce.PluginManager.add(
         "media", tinymce.plugins.MediaPlugin
     )
-})(); // This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/)
-{
-    of_beautifer();
-}
-else
-{
-    var a = b ? (c % d) : e[f];
-}
+})();
